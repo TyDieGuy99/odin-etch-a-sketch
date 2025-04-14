b7a63ed40cea3b768c4ee436b5022aa36629d528
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-04-14 01:22:55 -0400

    added for loop created for loop to create 256 divs + give mouse event

--- javascript.js
+++ javascript.js
@@ -1,13 +1,23 @@
-const body = document.querySelector('body');
+const container = document.querySelector('#container');
 
-const container = document.createElement('div');
-container.classList.add('container');
-body.appendChild(container);
-
-for (let i = 0; i < 16; i++) {
-    const boxi =document.createElement('div');
-    box.classList.add('box');
-    container.appendChild(box);
-
-    console.log(i + 1);
+for (let row = 0; row < 16; row++) {
+    for (let i = 0; i < 16; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.background = 'white';
+        box.style.border = '2px solid black';
+        box.style.width = '50px';
+        box.style.height = '50px';
+        container.appendChild(box);
+        box.addEventListener('mouseenter', (e) => {
+            if (box.style.background == 'white') {
+                box.style.background = 'black';
+                console.log(box.style.color);
+            } else {
+                box.style.background = 'white';
+                console.log(box.style.color);
+            }     
+        });
+        console.log((i + 1) * (row + 1))
+    }
 }
