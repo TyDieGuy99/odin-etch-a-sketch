b2e65470afb44ae371156ca66d8e340cbc0ba3b5
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-04-14 00:10:49 -0400

    for loop added for loop to make grid boxes

--- javascript.js
+++ javascript.js
@@ -3,3 +3,11 @@ const body = document.querySelector('body');
 const container = document.createElement('div');
 container.classList.add('container');
 body.appendChild(container);
+
+for (let i = 0; i < 16; i++) {
+    const boxi =document.createElement('div');
+    box.classList.add('box');
+    container.appendChild(box);
+
+    console.log(i + 1);
+}
