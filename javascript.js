b425c379d0cb2aeb4748023b7caf3a498c4cf876
TyDieGuy99 <120081348+TyDieGuy99@users.noreply.github.com>
2025-04-14 23:29:59 -0400

    random colors instead of black line does random colors

--- javascript.js
+++ javascript.js
@@ -13,17 +13,19 @@ function updateGrid() {
             box.style.width = boxSize;
             box.style.height = boxSize;
             container.appendChild(box);
-            box.addEventListener('mouseenter', (e) => {
-                if (box.style.background == 'white') {
-                    box.style.background = 'black';
-                    console.log(box.style.color);
-                }     
+            box.addEventListener('mouseenter', () => {
+                    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+                    var hexColor = '#' + randomColor
+                    box.style.background = hexColor;
+                    box.style.opacity = '0.10';
+                    console.log(box.style.background);
             });
             //console.log((i + 1) * (row + 1))
         }
     }
 }
 
+//call from button to create new grid
 function gridBtn() {
     console.log('button pressed');
     squares = prompt('Input a number 1-100 to create your grid size.');
@@ -31,10 +33,10 @@ function gridBtn() {
     updateGrid();
 }
 
+//clears current grid
 function clearGrid() {
     const parent = document.getElementById('container');
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
-    
 }
